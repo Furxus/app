4d8be83f28d2078bf29d179d7b2ba5c87354c102
Mateie <contact@mateie.dev>
2024-09-19 21:13:03 -0700

    main stuff

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,19 @@ import "@css/index.css";
 import "react-contexify/dist/ReactContexify.css";
 
 import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
 
 import MainProvider from "./providers/MainProvider";
 
 const container = document.getElementById("app");
 const root = createRoot(container!);
 
-root.render(<MainProvider />);
+if (import.meta.env.DEV === true) {
+    root.render(
+        <StrictMode>
+            <MainProvider />
+        </StrictMode>
+    );
+} else {
+    root.render(<MainProvider />);
+}
