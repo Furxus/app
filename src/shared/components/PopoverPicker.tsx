4fe02839d7ffc83a38e44b055d7953a4e87ec051
Mateie <business@mateie.dev>
2024-10-01 21:25:48 -0700

    styling adjustments for popover

--- src/shared/components/PopoverPicker.tsx
+++ src/shared/components/PopoverPicker.tsx
@@ -1,16 +1,18 @@
 import { useClickOutside } from "@/hooks";
 import { useCallback, useRef, useState } from "react";
 
-import { HexColorPicker } from "react-colorful";
+import { HexAlphaColorPicker } from "react-colorful";
 
 import "@css/ColorPicker.css";
 
 const PopoverPicker = ({
     color,
     onChange,
+    classNames,
 }: {
     color: string;
     onChange: any;
+    classNames?: string;
 }) => {
     const popover = useRef<any>();
     const [isOpen, toggle] = useState(false);
@@ -21,25 +23,28 @@ const PopoverPicker = ({
     return (
         <div className="relative">
             <div
-                className="w-7 h-7 rounded-lg border[3px] cursor-pointer"
+                className={`w-7 h-7 rounded-lg border[3px] cursor-pointer ${classNames}`}
                 onClick={() => toggle(true)}
                 style={{
                     boxShadow:
-                        "0 0 0 1px rgba(0, 0, 0, 0.1), inset 0 0 0 1px rgba(0, 0, 0, 0.1);",
+                        "0 0 0 1px rgba(0, 0, 0, 0.1), inset 0 0 0 1px rgba(0, 0, 0, 0.1)",
                     backgroundColor: color,
                 }}
             >
                 {isOpen && (
                     <div
-                        className="absolute right-1/2 transform translate-x-1/2 mt-2"
+                        className="absolute right-1/2 translate-y-[-50%] translate-x-[-20%]"
                         ref={popover}
                         style={{
                             top: "calc(100% + 2px)",
                             borderRadius: "9px",
-                            boxShadow: "0 6px 12px rgba(0, 0, 0, 0.15);",
+                            boxShadow: "0 6px 12px rgba(0, 0, 0, 0.15)",
                         }}
                     >
-                        <HexColorPicker color={color} onChange={onChange} />
+                        <HexAlphaColorPicker
+                            color={color}
+                            onChange={onChange}
+                        />
                     </div>
                 )}
             </div>
