85bb4ada3865039a2d4e6486f8343a6f65de8e24
Mateie <business@mateie.dev>
2024-10-13 15:41:12 -0700

    added alert for dms

--- src/dms/DMsLayout.tsx
+++ src/dms/DMsLayout.tsx
@@ -1,13 +1,25 @@
-import { Stack } from "@mui/material";
+import { Alert, Snackbar, Stack } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import SidebarDMs from "./components/SidebarDMs.component";
 
 const DMsLayout = () => {
     return (
-        <Stack direction="row" className="w-full h-full">
-            <SidebarDMs />
-            <Outlet />
-        </Stack>
+        <>
+            <Stack direction="row" className="w-full h-full">
+                <SidebarDMs />
+                <Outlet />
+            </Stack>
+            <Snackbar
+                anchorOrigin={{ vertical: "top", horizontal: "center" }}
+                open={true}
+            >
+                <Alert severity="warning" className="text-center">
+                    The DMs are currently being worked on, so some features may
+                    be missing :3 <br />
+                    <br /> Thank you for your patience
+                </Alert>
+            </Snackbar>
+        </>
     );
 };
 
