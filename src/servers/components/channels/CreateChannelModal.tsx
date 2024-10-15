e69d98b43bbd8946dce3ad49fc8a7d27ad9976e3
mateie <mateie@gmail.com>
2024-10-15 14:02:40 -0700

    prettified the modal

--- src/servers/components/channels/CreateChannelModal.tsx
+++ src/servers/components/channels/CreateChannelModal.tsx
@@ -3,12 +3,10 @@ import { CreateChannel } from "@gql/channels";
 import { Dispatch, SetStateAction, useState } from "react";
 import { FaHashtag } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
-import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import { Modal, Typography } from "@mui/material";
 
 const CreateChannelModal = ({
     serverId,
@@ -48,6 +46,63 @@ const CreateChannelModal = ({
     };
 
     return (
+        <Modal
+            className="flex items-center justify-center"
+            open={visible}
+            onClose={() => closeModal()}
+        >
+            <Stack
+                p={4}
+                gap={2}
+                direction="column"
+                alignItems="center"
+                className="bg-neutral-900 border rounded-lg w-1/5 border-green-500/60"
+            >
+                <Typography variant="h6">Create a channel</Typography>
+                <Stack direction="column" gap={2} alignItems="center">
+                    <Stack direction="column" gap={1}>
+                        <TextField
+                            color="success"
+                            onChange={(e) =>
+                                setFields((prev) => ({
+                                    ...prev,
+                                    name: e.target.value,
+                                }))
+                            }
+                            name="name"
+                            label="Channel Name"
+                            value={fields.name}
+                            error={!!errors.name}
+                            helperText={errors.name}
+                            slotProps={{
+                                input: {
+                                    startAdornment: (
+                                        <FaHashtag className="text-gray-500 mr-2" />
+                                    ),
+                                },
+                            }}
+                            onKeyDown={(e) => {
+                                if (e.key === "Enter" && fields.name)
+                                    createChannel();
+                            }}
+                        />
+                        <Button
+                            color="success"
+                            onClick={() => {
+                                createChannel();
+                            }}
+                            variant="contained"
+                            disabled={loading || !fields.name}
+                        >
+                            Create Channel
+                        </Button>
+                    </Stack>
+                </Stack>
+            </Stack>
+        </Modal>
+    );
+
+    /*return (
         <Dialog open={visible} onClose={closeModal}>
             <DialogTitle className="flex gap-2 items-center justify-center">
                 <h2 className="text-lg font-semibold">Create a channel</h2>
@@ -96,7 +151,7 @@ const CreateChannelModal = ({
                 </Stack>
             </DialogContent>
         </Dialog>
-    );
+    );*/
 };
 
 export default CreateChannelModal;
